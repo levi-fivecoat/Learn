64d9f856a05d6e6c899cc3a828756280287776d5
levi-fivecoat <levi.fivecoat55@gmail.com>
2021-08-29 19:43:43 -0600

    TIME: 1M

--- Datatypes/lists.py
+++ Datatypes/lists.py
@@ -5,7 +5,10 @@ things = [1, 2.3, "my_str", [1, 2, 3], "Hello World", {"hats": 2000}]
 for thing in things:
     print(thing)
 
+# ADD SOMETHING TO YOUR LIST
 things.append("ANOTHER THING")
 print(things)
+
+# REMOVE SOMETHING FROM YOUR LIST
 things.remove([1, 2, 3])
 print(things)
