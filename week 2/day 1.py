0d245aa264e29ba5965ff4cb8549dc7cd8bbb4d6
levi-fivecoat <levi.fivecoat55@gmail.com>
2021-08-31 18:21:33 -0600

    time spent 1 hour

---
+++ week 2/day 1.py
@@ -0,0 +1,14 @@
+coordinates = [1, 2, 3]
+x, y, z = coordinates
+print (y)
+
+#30 minutes
+
+customer = {
+    "name": "Levi fivecoat",
+    "age": 13,
+    "is_verified": True
+}
+print (customer.get("name"))
+
+#first hour
