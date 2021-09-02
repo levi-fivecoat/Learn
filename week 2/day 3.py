c2ae4ba13798883417a9ff2111cbc634d5005438
levi-fivecoat <levi.fivecoat55@gmail.com>
2021-09-02 09:23:20 -0600

    1 hour

---
+++ week 2/day 3.py
@@ -0,0 +1,11 @@
+#30 minutes
+
+def greet_user(first_name, last_name):
+    print(f'hi{first_name} {last_name}!")
+    print("welcome aboard")
+
+#1 hour
+def square(number):
+    return number * number
+
+print(square(3))
